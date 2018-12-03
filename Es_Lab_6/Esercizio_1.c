5502f26c63df38a91d3ffd1d65b06afd8dabd046
IfNotFalseTrue <39000487+IfNotFalseTrue@users.noreply.github.com>
2018-12-03 16:44:51 +0100

    Create Esercizio_1.c

---
+++ Es_Lab_6/Esercizio_1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
+#define N 5
+
+
+static char is_palindrome(int *a, size_t len)
+{
+    size_t i = 0;
+    char palindrome = TRUE;
+    len -= 1;
+
+    for(i = 0; i < len/2 && palindrome; ++i) {
+        if(a[i] != a[len - i]) {
+            palindrome = FALSE;
+        }
+    }
+
+    return palindrome;
+}
+
+
+int main(void)
+{   
+    int v[N];
+    size_t i = 0;
+
+    for(i = 0; i < N; ++i) {
+        scanf("%d", &v[i]);
+    }
+
+    printf("Il vettore inserito %se' palindromo\n", is_palindrome(v, N) ? "" : "non ");
+
+    return EXIT_SUCCESS;
+}
+
+
