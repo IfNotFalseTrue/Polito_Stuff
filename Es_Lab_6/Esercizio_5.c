d38fe617814ecb0dea56139cacba56ca7a4b5dce
IfNotFalseTrue <39000487+IfNotFalseTrue@users.noreply.github.com>
2018-12-03 17:14:29 +0100

    Create Esercizio_5.c

---
+++ Es_Lab_6/Esercizio_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static int int_power(int base, int exponent)
+{
+    int result = 1;
+    while(exponent != 0) {
+        if((exponent & 1) != 0) {
+            result *= base;
+        }
+        exponent >>= 1;
+        base *= base;
+    }
+
+    return result;
+}
+
+
+int main(void)
+{
+    int a , b;
+    
+    scanf("%d%d", &a, &b);
+    printf("%d\n", int_power(a, b));
+
+    return EXIT_SUCCESS;
+}
+
