dd575f1534abd2f89f3ed0bf2e2463843b856805
IfNotFalseTrue <39000487+IfNotFalseTrue@users.noreply.github.com>
2018-12-03 17:10:03 +0100

    Create Esercizio_2.c

---
+++ Es_Lab_6/Esercizio_2.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
+#define N 5
+
+
+static void xor_swap(int *a, int *b)
+{
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
+static void input_array(int *a, size_t len)
+{
+    size_t i;
+    
+    for(i = 0; i < len; ++i) {
+        scanf("%d", &a[i]);
+    }
+}
+
+static void bubble_sort(int *v, size_t len)
+{
+    size_t i, j;
+    char swapped = TRUE;
+    len -= 1;
+
+    for(i = 0; i < len && swapped; ++i) {
+        swapped = FALSE;
+        for(j = 0; j < len - i; ++j) {
+            if(v[j] > v[j + 1]) {
+                xor_swap(&v[j], &v[j + 1]);
+                swapped = TRUE;
+            }
+        }
+    }
+}
+
+
+static char cmp_arrays(int *v1, int *v2, size_t len)
+{
+    size_t i = 0;
+    char equals = TRUE;
+
+    for(i = 0; i < len && equals; ++i) {
+        if(v1[i] != v2[i]) {
+            equals = FALSE;
+        }
+    }
+
+    return equals;
+}
+
+
+int main(void)
+{
+    int v1[N];
+    int v2[N];
+
+    input_array(v1, N);
+    input_array(v2, N);
+
+    bubble_sort(v1, N);
+    bubble_sort(v2, N);
+
+    printf("I 2 vettori %scontengono gli stessi elementi\n",
+            cmp_arrays(v1, v2, N) ? "" : "non ");
+
+    return EXIT_SUCCESS;
+}
+
