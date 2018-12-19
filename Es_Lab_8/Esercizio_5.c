62778ca962f8233b0513d8ac53ffb463b801b370
IfNotFalseTrue <39000487+IfNotFalseTrue@users.noreply.github.com>
2018-12-19 21:02:53 +0100

    Create Esercizio_5.c

---
+++ Es_Lab_8/Esercizio_5.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_S 64
+#define N 3
+
+static int search_product
+(const char haystack[N][MAX_S], const char *needle, int dim)
+{
+    int i;
+    int index = -1;
+
+    for(i = 0; i < dim && index == -1; ++i) {
+        if(strcmp(haystack[i], needle) == 0) {
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+
+int main(void)
+{
+    char input[MAX_S];
+    char products[N][MAX_S];
+    double price[N];
+    char my_product[MAX_S];
+    int prod_index, i;
+    unsigned int qtity = 0;
+
+    printf("Inserisci %d prodotti e prezzi separati da spazio:\n", N);
+    for(i = 0; i < N; ++i) {
+        gets(input);
+        if(sscanf(input, "%s %lf", products[i], &price[i]) != 2) {
+            puts("Errore nell'input...");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    do {
+        puts("Inserisci il prodotto da cercare e la quantita':");
+        gets(input);
+        sscanf(input, "%s %u", my_product, &qtity);
+        prod_index = search_product(products, my_product, N);
+        if(prod_index == -1) {
+            puts("Prodotto non trovato");
+        }
+        else {
+            printf("Prezzo totale di %s: %.2lf\n", my_product, qtity * price[prod_index]);
+        }
+
+    } while(prod_index == -1);
+
+    return EXIT_SUCCESS;
+}
