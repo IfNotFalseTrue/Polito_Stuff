b157d206814ba4ec5a56daf816d0e840ebdb75d0
IfNotFalseTrue <39000487+IfNotFalseTrue@users.noreply.github.com>
2019-11-12 02:28:53 +0100

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,26 +1,26 @@
 /*
-Successione di Fibonacci.
-Non tiene conto di eventuali overflow, perché sono pigro.
+Fibonacci sequence iterative in linear time.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_N 20 - 1
+#include <inttypes.h>
+#define MAX_N 93    // We can't go beyond that! 
 
 
 int main(void)
 {
-    int a[2] = {0, 1};
-    int i;
+    uint64_t a[2] = {0, 1};
+    size_t i;
 
-    printf( "N°[0] = %d\n"
-            "N°[1] = %d\n",
+    printf( "N°[0] = %" PRIu64 "\n"
+            "N°[1] = %" PRIu64 "\n",
             a[0], a[1]);
 
-    for (i = 0; i < MAX_N; ++i) {
-        int j = i & 1;
+    for (i = 0; i < MAX_N - 1; ++i) {
+        uint64_t j = i & 1;
         a[j] += a[j ^ 1];
-        printf("N°[%d] = %d\n", i + 2, a[j]);
+        printf("N°[%" PRIu64 "] = %" PRIu64 "\n", i + 2, a[j]);
     }
 
     return EXIT_SUCCESS;
